925200db1bc8f370a6e66ad533f31da6fb794e92
lastre <orionbl@hotmail.es>
2016-12-24 13:25:32 +0100

    Python script run

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,42 @@
 #include <Python.h>
 #include <iostream>
+#include "pyCpp.h"
 //https://www.codeproject.com/Articles/820116/Embedding-Python-program-in-a-C-Cplusplus-code
 
-void checkPython ()
+void testEmbeddedPython()
 {
 	Py_Initialize();
 	PyRun_SimpleString("print('Python is running correctly')");
 	Py_Finalize();
 }
 
+void testPyCPPInstance()
+{
+	PyCppInstance pyInstance;
+	PyRun_SimpleString("print('PyCppInstance class helper is running correctly')");
+}
+
+void testPythonScript()
+{
+	FILE* file = _Py_fopen("script1.py", "r");
+	//FILE* file = fopen("script1.py", "r");
+	if (file==NULL)
+	{
+		std::cout <<  "Error opening script.py" << std::endl;
+		return;
+	}
+	PyRun_SimpleFile(file, "script1");
+}
+
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-	checkPython();
+	testEmbeddedPython();
+	testPyCPPInstance();
+	
+	PyCppInstance pyInstance;
+	testPythonScript();
+
     return 0;
 }
